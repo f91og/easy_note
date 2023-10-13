63cbe8d3cab5333628f8a74018612fd96a451dcd
f91og <yuxuef91og@gmail.com>
2023-10-13 16:30:02 +0900

    ~change: hide macos window left-top control button and title bar

--- macos/Runner/MainFlutterWindow.swift
+++ macos/Runner/MainFlutterWindow.swift
@@ -8,6 +8,17 @@ class MainFlutterWindow: NSWindow {
     self.contentViewController = flutterViewController
     self.setFrame(windowFrame, display: true)
 
+    self.standardWindowButton(.miniaturizeButton)?.isHidden = true // 隐藏最小化按钮
+    self.standardWindowButton(.zoomButton)?.isHidden = true // 隐藏最大化按钮
+    self.standardWindowButton(.closeButton)?.isHidden = true // 隐藏关闭按钮
+
+    // 隐藏标题栏
+    self.titlebarAppearsTransparent = true
+    self.titleVisibility = .hidden
+
+    // 完全隐藏标题栏
+    self.styleMask = [.borderless, .fullSizeContentView]
+
     RegisterGeneratedPlugins(registry: flutterViewController)
 
     super.awakeFromNib()
